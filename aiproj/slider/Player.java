5fedbcc22768dac346b85312b3c85fac0c6806e0
samchungy <samchungy@gmail.com>
2017-05-09 02:17:35 +1000

    Attempted Mini Max, need to discuss the rewind thingo

--- aiproj/slider/Player.java
+++ aiproj/slider/Player.java
@@ -17,7 +17,10 @@ import java.util.Random;
 public class Player implements SliderPlayer {
     private int d;
     private String b[];
+    /*Player*/
     private char p;
+    /*Opponent*/
+    private char o;
 
     private Board gameBoard;
 
@@ -27,6 +30,12 @@ public class Player implements SliderPlayer {
         this.d = dimension;
         this.b = board.split("\n");
         this.p = player;
+        if (player == 'V'){
+            this.o = 'H';
+        }
+        else{
+            this.o = 'V';
+        }
 
         this.gameBoard = new Board(dimension, this.b);
 
@@ -94,11 +103,59 @@ public class Player implements SliderPlayer {
         return move;
     }
 
+
+
     /**
      * Attempt at depth limited minimax
      * @return move
      */
-    private Move minimax(Board board, Move move, int depth, boolean maximizingPlayer) {
+    private int minimax(Board board, Move move, int depth, boolean maximizingPlayer) {
+        int score = evaluateMove(move);
+
+        /*APPLY MOVE - NEED TO IMPLEMENT (ALSO NEED TO DISCUSS THE REWIND MOVE THINGO*/
+
+        // Base Case 1 - Reached Specified Depth of Search
+        // Base Case 2 - Reached End of Game (No player V or H locations
+        if (depth == 0 || board.getPlayerHLocations().size() == 0 || board.getPlayerVLocations().size() == 0)
+            return score;
+
+        if (maximizingPlayer){
+            int max = 0;
+            int i = 0;
+            List<Move> possMoves = generateMoves(this.p);
+            int listSize = possMoves.size();
+            max = minimax(board, possMoves.get(i), depth--, !maximizingPlayer);
+            i++;
+            // Return highest score of moves.
+            for(;i<listSize;i++){
+                int moveScore = minimax(board, possMoves.get(i), depth--, !maximizingPlayer);
+                if(moveScore > max){
+                    max = moveScore;
+                }
+            }
+            return max;
+        }
+        //CAN PROBABLY JUST MERGE THESE 2 BUT IM TIRED
+        else{
+            int min = 0;
+            int i = 0;
+            List<Move> possMoves = generateMoves(this.p);
+            int listSize = possMoves.size();
+            min = minimax(board, possMoves.get(i), depth--, !maximizingPlayer);
+            i++;
+            for(;i<listSize;i++){
+                int moveScore = minimax(board, possMoves.get(i), depth--, !maximizingPlayer);
+                if(moveScore < min){
+                    min = moveScore;
+                }
+            }
+            return min;
+        }
+
+
+
+
+
 //        01 function minimax(node, depth, maximizingPlayer)
 //        02     if depth = 0 or node is a terminal node
 //        03         return the heuristic value of node
@@ -155,7 +212,7 @@ public class Player implements SliderPlayer {
 //                bestMove = currentMove;
 //            }
 //        }
-        return null;
+        return 0;
     }
 
     /**
@@ -268,7 +325,7 @@ public class Player implements SliderPlayer {
      * @return heuristic value
      */
     private int evaluateMove(Move move) {
-        int score = 0;
+        int score;
         int myMobility = 0;
         int oppMobility = 0;
 
@@ -279,32 +336,7 @@ public class Player implements SliderPlayer {
             myMobility = numLegalMoves('V', this.gameBoard);
             oppMobility = numLegalMoves('H', this.gameBoard);
         }
-
-
-
-        // cell that is being moved away from
-        Cell cell = this.gameBoard.getBoard()[move.i][move.j];
-        char tmpCellChar = cell.getPieceTypeChar();
-
-        // cell that is being moved to
-        int to_i = move.i, to_j = move.j;
-        switch(move.d) {
-            case UP:    to_j++; break;
-            case DOWN:  to_j--; break;
-            case RIGHT: to_i++; break;
-            case LEFT:  to_i--; break;
-        }
-
-        // moving to a location which is off the board?
-        if (cell.getPieceTypeChar() == 'H' && to_i == this.d) {
-            // score value of finishing off a piece?
-        }
-        if (cell.getPieceTypeChar() == 'V' && to_j == this.d) {
-            // score value of finishing off a piece?
-        }
-
-
-
+        score = myMobility-oppMobility;
 
         return score;
     }
