82edea7a813a4b2ed3a9b1ab6cb2c99dc00b6c83
samchungy <samchungy@gmail.com>
2017-05-15 21:53:22 +1000

    Added 2 different comparators for V & H respectively

--- aiproj/slider/agents/helper/MoveListComparator.java
+++ aiproj/slider/agents/helper/MoveListComparator.java
@@ -7,43 +7,78 @@ import java.lang.Math;
 /**
  * Created by SamCh on 14/05/2017.
  */
-public class MoveListComparator implements Comparator<Move> {
-    public int compare (Move move1, Move move2){
-        //Preference pieces moving towards the top right corner.
-        if(move1.d == Move.Direction.RIGHT || move1.d == Move.Direction.UP){
-            if(move2.d == Move.Direction.RIGHT || move2.d == Move.Direction.UP){
-                //Preference pieces which are closer to (0,0)
-                if(Distance(move1.i,move1.j) < Distance(move2.i, move2.j)){
+public class MoveListComparator{
+
+    public static Comparator<Move> HComparator = new Comparator<Move>(){
+
+        public int compare (Move move1, Move move2){
+            //If move1 is headed right
+            if(move1.d == Move.Direction.RIGHT){
+                if(move2.d != Move.Direction.RIGHT){
                     return -1;
                 }
-                else{
+            }
+            //If move1 is headed up
+            if (move1.d == Move.Direction.UP){
+                if (move2.d == Move.Direction.RIGHT){
                     return 1;
                 }
+                else if (move2.d != Move.Direction.UP){
+                    return -1;
+                }
             }
-            else{
-                return -1;
-            }
-        }else{
-            if(move2.d == Move.Direction.RIGHT || move2.d == Move.Direction.UP){
+            //If move2 is headed up or right
+            if (move2.d == Move.Direction.UP || move2.d == Move.Direction.RIGHT){
                 return 1;
             }
+            //Neither is headed right or up, check the distance.
+            if(Distance(move1.i,move1.j) < Distance(move2.i, move2.j)){
+                return -1;
+            }
             else{
-                if(Distance(move1.i,move1.j) < Distance(move2.i, move2.j)){
-                    return -1;
-                }
-                else{
-                    return 1;
-                }
+                return 1;
             }
         }
 
-    }
+    };
+    public static Comparator<Move> VComparator = new Comparator<Move>(){
+
+        public int compare (Move move1, Move move2){
+            //If move1 is headed up
+            if(move1.d == Move.Direction.UP){
+                if(move2.d != Move.Direction.UP){
+                    return -1;
+                }
+            }
+            //If move1 is headed right
+            if (move1.d == Move.Direction.RIGHT){
+                if (move2.d == Move.Direction.UP){
+                    return 1;
+                }
+                else if (move2.d != Move.Direction.RIGHT){
+                    return -1;
+                }
+            }
+            //If move2 is headed up or right
+            if (move2.d == Move.Direction.UP || move2.d == Move.Direction.RIGHT){
+                return 1;
+            }
+            //Neither is headed right or up, check the distance.
+            if(Distance(move1.i,move1.j) < Distance(move2.i, move2.j)){
+                return -1;
+            }
+            else{
+                return 1;
+            }
+        }
+
+    };
     /**
      * Returns distance from (0,0)
      * @param x X cordinate, @param y Y coordinate
      * @return distance
      */
-    private double Distance(int x, int y){
+    private static double Distance(int x, int y){
         return Math.sqrt(x*x+y*y);
     }
 
