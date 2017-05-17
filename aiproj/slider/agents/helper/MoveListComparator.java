bac30c9582bcfaeeacb448350a5458aba6b66aea
Hao Le <leh2@student.unimelb.edu.au>
2017-05-17 14:47:24 +1000

    Cleaned up MoveListComparator

--- aiproj/slider/agents/helper/MoveListComparator.java
+++ aiproj/slider/agents/helper/MoveListComparator.java
@@ -1,24 +1,32 @@
+/* Hao Le - leh2
+ * Sam Chung - chungs1
+ * 
+ * MoveListComparator Class
+ * Compares the moves which are available to player
+ * Used for sorting the moves
+ * Last Modified: 17/05/17
+ *
+ */
+
 package aiproj.slider.agents.helper;
 import aiproj.slider.Move;
 
 import java.util.Comparator;
 import java.lang.Math;
 
-/**
- * Created by SamCh on 14/05/2017.
- */
 public class MoveListComparator{
 
     public static Comparator<Move> HComparator = new Comparator<Move>(){
 
         public int compare (Move move1, Move move2){
-            //If move1 is headed right
+            // if move1 is headed right
             if(move1.d == Move.Direction.RIGHT){
                 if(move2.d != Move.Direction.RIGHT){
                     return -1;
                 }
             }
-            //If move1 is headed up
+
+            // if move1 is headed up
             if (move1.d == Move.Direction.UP){
                 if (move2.d == Move.Direction.RIGHT){
                     return 1;
@@ -27,11 +35,13 @@ public class MoveListComparator{
                     return -1;
                 }
             }
-            //If move2 is headed up or right
+
+            // if move2 is headed up or right
             if (move2.d == Move.Direction.UP || move2.d == Move.Direction.RIGHT){
                 return 1;
             }
-            //Neither is headed right or up, check the distance.
+            
+            // neither is headed right or up, check the distance.
             if(Distance(move1.i,move1.j) < Distance(move2.i, move2.j)){
                 return -1;
             }
@@ -44,13 +54,14 @@ public class MoveListComparator{
     public static Comparator<Move> VComparator = new Comparator<Move>(){
 
         public int compare (Move move1, Move move2){
-            //If move1 is headed up
+            // if move1 is headed up
             if(move1.d == Move.Direction.UP){
                 if(move2.d != Move.Direction.UP){
                     return -1;
                 }
             }
-            //If move1 is headed right
+            
+            //if move1 is headed right
             if (move1.d == Move.Direction.RIGHT){
                 if (move2.d == Move.Direction.UP){
                     return 1;
@@ -59,11 +70,13 @@ public class MoveListComparator{
                     return -1;
                 }
             }
-            //If move2 is headed up or right
+            
+            //if move2 is headed up or right
             if (move2.d == Move.Direction.UP || move2.d == Move.Direction.RIGHT){
                 return 1;
             }
-            //Neither is headed right or up, check the distance.
+            
+            // neither is headed right or up, check the distance.
             if(Distance(move1.i,move1.j) < Distance(move2.i, move2.j)){
                 return -1;
             }
